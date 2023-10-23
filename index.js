a4ca527e941fd77a31ac08c211158f1c27b5dea1
StanoSpodniak <stano28@gmail.com>
2023-10-23 12:37:55 +0200

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-//https://www.youtube.com/watch?v=kgCjXjJkZ-Y&list=PLC3y8-rFHvwiaOAuTtVXittwybYIorRB3&index=13
+//https://www.youtube.com/watch?v=uN-gpihDQRE&list=PLC3y8-rFHvwiaOAuTtVXittwybYIorRB3&index=15
 const redux = require('redux')
 const createStore = redux.createStore
 const bindActionCreators = redux.bindActionCreators
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+
+const reduxLogger = require('redux-logger')
+const logger = reduxLogger.createLogger()
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
@@ -91,12 +95,10 @@ const rootReducer = combineReducers({
     iceCream: iceCreamReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('Initial state', store.getState())
 
-const unsubscribe = store.subscribe(() => 
-    console.log('Update state ', store.getState())
-)
+const unsubscribe = store.subscribe(() => {})
 
 // store.dispatch(orderCake())
 // store.dispatch(orderCake())
